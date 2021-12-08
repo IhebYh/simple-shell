6f66ecff31d6e407da50080af67c49d3c15254c8
Iheb Yahyaoui <iheb.yah@gmail.com>
2021-12-08 01:11:52 +0100

    main entry point

---
+++ shell.c
@@ -0,0 +1,36 @@
+#include "shell.h"
+/**
+ * main - entry point
+ * @argc : int
+ * @argv: char
+ * Return: int
+ */
+int main(__attribute__((unused)) int argc, char **argv)
+{
+	char *str = NULL;
+	size_t count = 0;
+	ssize_t gl = 0;
+
+	while (INFINITE_LOOP)
+	{
+		write(STDOUT_FILENO, "$ ", 2);
+		gl = getline(&str, &count, stdin);
+
+		if (gl == EOF)
+		{
+			free(str);
+			exit(0);
+		}
+		if (_strcmp(str, "\n"))
+		{
+			cmd_checker(str);
+			str = NULL;
+		}
+		else
+		{
+			free(str);
+			str = NULL;
+		}
+	}
+		return (0);
+}
